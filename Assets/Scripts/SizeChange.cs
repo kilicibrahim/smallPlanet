f6e261365de9d770fd7af9089385ad158d537e77
kilicibrahim <muhammedibrahimkilic@gmail.com>
2022-08-17 12:48:52 +0300

    SizeChange

--- Assets/Scripts/SizeChange.cs
+++ Assets/Scripts/SizeChange.cs
@@ -12,6 +12,7 @@ public class SizeChange : MonoBehaviour
     int eatScore;
     private float displaySize = 3000;
     public static bool isEaten = false;
+    public static bool itEats = false;
 
     private PlayerJoyStickMovement playerJoyStickMovement;
     private void Start(){
@@ -21,20 +22,29 @@ public class SizeChange : MonoBehaviour
     private void FixedUpdate() {
     transform.localScale  += sizeChange;
     size += sizeChanger;
+
+    if(SizeChange.itEats == true){
+            size += playerSize;
+            transform.localScale = new Vector3(size, size, size);
+            SizeChange.itEats = false;
+        }
     }
     private void Update(){
         //AIsize = sizeChange.size;
         playerSize = playerJoyStickMovement.size;
         displaySize = size *1000;
+
+        
     }
 
      private void OnCollisionEnter(Collision collision) {
         if(collision.gameObject.tag.Equals("Player"))
         {
-            if(AIsize >  playerSize){
-                Destroy(collision.gameObject);       
+            if(size >  playerSize){
+                Destroy(collision.gameObject);
+                SizeChange.itEats = true;       
         }
-            else if(playerSize > AIsize){
+            else if(playerSize > size){
                 Destroy(gameObject);
                 eatScore +=1;
                 isEaten = true;
